fixed delta FT normalization

density_profiles.py
@@ -536,12 +536,16 @@ def profile_delta(r_range, m_range):
     '''
     Returns a delta function profile
     '''
-    profile = np.zeros_like(r_range)
+    profile = np.zeros_like(r_range, dtype=float)
 
     profile[:,0] = 1.
     profile *= m_range.reshape(-1,1)
 
     return profile
+
+# ------------------------------------------------------------------------------
+# End of profile_delta()
+# ------------------------------------------------------------------------------
 
 def profile_delta_f(k_range, m_range):
     '''
@@ -560,11 +564,14 @@ def profile_delta_f(k_range, m_range):
     profile_f : (m,k) array
       array containing Fourier transform of delta profile
     '''
-    profile = np.ones_like(m_range.shape + k_range.shape)
-
-    profile *= 1./m_range.reshape(-1,1)
+    profile = np.ones(m_range.shape + k_range.shape, dtype=float)
+    # profile /= m_range.reshape(-1,1)
 
     return profile
+
+# ------------------------------------------------------------------------------
+# End of profile_delta_f()
+# ------------------------------------------------------------------------------
 
 def profile_sersic(r_range, m_range, r_eff, p, q=1):
     '''

main.py
@@ -58,7 +58,7 @@ def load_dm():
 # ------------------------------------------------------------------------------
 
 def load_sat():
-    f_sat = stars.f_s(p.rpms.m_range_lin)
+    f_sat = stars.f_s(p.prms.m_range_lin)
     # Satellite profile
     # generalized NFW profile determined by van der Burg, Hoekstra et al. (2015)
     prof_sat = profs.profile_gNFW(prms.r_range_lin, c_x=0.64*np.ones(prms.m_bins),
@@ -87,27 +87,28 @@ def load_bcg():
     f_cen = stars.f_c(p.prms.m_range_lin)
     # BCG profile
     # Kravtsov (2014) -> r1/2 = 0.015 r200
-    # prof_bcg = profs.profile_BCG(prms.r_range_lin, prms.m_range_lin,#st.m_cen_fit,
-    #                              r_half=0.015*prms.r_range_lin[:,-1])
+    prof_bcg = profs.profile_BCG(prms.r_range_lin, prms.m_range_lin,#st.m_cen_fit,
+                                 r_half=0.015*prms.r_range_lin[:,-1])
 
     # quite arbitrary...
-    # n1_idx = (prms.m_range_lin * f_cen < 1e11)
-    # n4_idx = (prms.m_range_lin * f_cen >= 1e11)
+    n1_idx = (prms.m_range_lin * f_cen < 1e11)
+    n4_idx = (prms.m_range_lin * f_cen >= 1e11)
 
-    # prof_bcg_n1 = profs.profile_sersic(prms.r_range_lin[n1_idx],
-    #                                    prms.m_range_lin[n1_idx],
-    #                                    r_eff=0.015*prms.r_range_lin[n1_idx,-1],
-    #                                    p=1)
-    # prof_bcg_n4 = profs.profile_sersic(prms.r_range_lin[n4_idx],
-    #                                    prms.m_range_lin[n4_idx],
-    #                                    r_eff=0.015*prms.r_range_lin[n4_idx,-1],
-    #                                    p=4)
-    # prof_bcg = np.concatenate([prof_bcg_n1, prof_bcg_n4], axis=0)
+    prof_bcg_n1 = profs.profile_sersic(prms.r_range_lin[n1_idx],
+                                       prms.m_range_lin[n1_idx],
+                                       r_eff=0.015*prms.r_range_lin[n1_idx,-1],
+                                       p=1)
+    prof_bcg_n4 = profs.profile_sersic(prms.r_range_lin[n4_idx],
+                                       prms.m_range_lin[n4_idx],
+                                       r_eff=0.015*prms.r_range_lin[n4_idx,-1],
+                                       p=4)
+    prof_bcg = np.concatenate([prof_bcg_n1, prof_bcg_n4], axis=0)
 
-    prof_bcg = profs.profile_delta(p.prms.r_range_lin, p.prms.m_range_lin)
-    prof_bcg_f = profs.profile_delta_f(p.prms.k_range_lin, p.prms.m_range_lin)
+    # prof_bcg = profs.profile_delta(p.prms.r_range_lin, p.prms.m_range_lin)
+    # prof_bcg_f = profs.profile_delta_f(p.prms.k_range_lin, p.prms.m_range_lin)
     bcg_extra = {'profile': prof_bcg,
-                 'profile_f': prof_bcg_f}
+                 'profile_f' : None}
+                 # 'profile_f': prof_bcg_f}
     prof_bcg_kwargs = tools.merge_dicts(profile_kwargs, bcg_extra)
 
     print '! Check M_h-M* relation !'

model/component.py
@@ -13,7 +13,7 @@ import pdb
 class Component(dens.Profile):
     '''
     An object containing information about a matter component.
-    
+
     Parameters
     ----------
     name : str
@@ -26,11 +26,11 @@ class Component(dens.Profile):
       fractional contribution of component to total mass for an object of
       equivalent mass m at redshift z
     bias_fn : function or array
-      function to compute the bias (should have first arg for m_range) or array 
+      function to compute the bias (should have first arg for m_range) or array
       containing profile with m along axis 0
     bias_fn_args : dict
       dictionary containing the additional arguments to the bias function
-    
+
     Methods
     -------
     P_1h : (k,) array
@@ -59,7 +59,7 @@ class Component(dens.Profile):
 
     #===========================================================================
     # Parameters
-    #===========================================================================    
+    #===========================================================================
     # @parameter
     # def dndlnm(self, val):
     #     return val
@@ -76,9 +76,9 @@ class Component(dens.Profile):
     def m_fn(self, val):
         if not isinstance(val, hmf.MassFunction):
             raise TypeError('m_fn should be hmf.MassFunction instance.')
-        else: 
+        else:
             return val
-    
+
     @parameter
     def f_comp(self, val):
         return val
@@ -86,14 +86,14 @@ class Component(dens.Profile):
     @parameter
     def bias_fn(self, val):
         return val
-    
+
     @parameter
     def bias_fn_args(self, val):
         return val
 
     #===========================================================================
     # Methods
-    #===========================================================================    
+    #===========================================================================
     @cached_property('m_range', 'f_comp', 'm_fn')
     def rho_comp(self):
         '''
@@ -115,7 +115,7 @@ class Component(dens.Profile):
 
         if len(b.shape) != 1:
             raise ValueError('bias_fn should be an (m,) array. ')
-                
+
         else:
             # correct bias such that P_2h -> P_lin for k -> 0
             # this correction assumes Simpson integration!!!
@@ -149,7 +149,7 @@ class Component(dens.Profile):
                 b[0] += (diff * 6./(hsum * (2 - 1.0/h0divh1)) *
                          1./(dndlnm[0] * self.f_comp[0]))
 
-            
+
             bias_int = Integrate(y=dndlnm * b * self.f_comp,
                                  x=self.m_range,
                                  axis=0)
@@ -167,7 +167,7 @@ class Component(dens.Profile):
           array containing halo mass for each profile
         '''
         m = self.m_range.shape[0]
-        
+
         f_comp = self.f_comp.reshape(m,1)
         return 4*np.pi * Integrate(self.rho_r * f_comp * self.r_range**2,
                                    self.r_range,
@@ -181,7 +181,7 @@ class Component(dens.Profile):
         # define shapes for readability
         m = self.m_range.shape[0]
         k = self.k_range.shape[0]
-        
+
         dndlnm = self.m_fn.dndlnm.reshape(m,1)
         m_range = self.m_range.reshape(m,1)
         f_comp = self.f_comp.reshape(m,1)
@@ -194,7 +194,7 @@ class Component(dens.Profile):
         result *= prefactor
 
         return result
-        
+
     @cached_property('m_range', 'k_range', 'm_fn', 'rho_comp', 'rho_k')
     def P_2h(self):
         '''
@@ -203,7 +203,7 @@ class Component(dens.Profile):
         # define shapes for readability
         m = self.m_range.shape[0]
         k = self.k_range.shape[0]
-        
+
         dndlnm = self.m_fn.dndlnm.reshape(m,1)
         m_range = self.m_range.reshape(m,1)
         f_comp = self.f_comp.reshape(m,1)
@@ -226,7 +226,7 @@ class Component(dens.Profile):
     def Delta_1h(self):
         '''
         Return the dimensionless power spectrum for 1-halo term of component
-        
+
                Delta[k] = 1/(2*pi^2) * k^3 P(k)
         '''
         return 1./(2*np.pi**2) * self.k_range**3 * self.P_1h
@@ -235,7 +235,7 @@ class Component(dens.Profile):
     def Delta_2h(self):
         '''
         Return the dimensionless power spectrum for 2-halo term of component
-        
+
                Delta[k] = 1/(2*pi^2) * k^3 P(k)
         '''
         return 1./(2*np.pi**2) * self.k_range**3 * self.P_2h
@@ -248,108 +248,107 @@ class Component(dens.Profile):
 # End of Component()
 # ------------------------------------------------------------------------------
 
-# class DiffuseComponent(Cache):
-#     '''
-#     An object containing information about a diffuse matter component. This 
-#     component traces the linear matter power spectrum with a bias factor.
-    
-#     Parameters
-#     ----------
-#     name : str
-#       name of the component
-#     m_fn : hmf.MassFunction object
-#       the halo mass function
-#     f_comp : (m,) array
-#       fractional contribution of component to total mass for an object of
-#       equivalent mass m at redshift z
-#     bias : float
-#       bias factor between diffuse component and linear power spectrum
+class DiffuseComponent(Cache):
+    '''
+    An object containing information about a diffuse matter component. This
+    component traces the linear matter power spectrum with a bias factor.
 
-#     Methods
-#     -------
-#     P_2h : (k,) array
-#       2 halo term of power spectrum
-#     Delta_2h : (k,) array
-#       2 halo term of dimensionless power spectrum
-#     '''
-#     def __init__(self, name, m_range, k_range, m_fn, f_comp, bias):
-#         super(DiffuseComponent, self).__init__()
-#         self.name = name
-#         self.m_range = m_range
-#         self.k_range = k_range
-#         self.m_fn = m_fn
-#         self.f_comp = f_comp
-#         self.bias = bias
+    Parameters
+    ----------
+    name : str
+      name of the component
+    m_fn : hmf.MassFunction object
+      the halo mass function
+    f_comp : (m,) array
+      fractional contribution of component to total mass for an object of
+      equivalent mass m at redshift z
+    bias : float
+      bias factor between diffuse component and linear power spectrum
 
-#     #===========================================================================
-#     # Parameters
-#     #===========================================================================    
-#     @parameter
-#     def name(self, val):
-#         return val
+    Methods
+    -------
+    P_2h : (k,) array
+      2 halo term of power spectrum
+    Delta_2h : (k,) array
+      2 halo term of dimensionless power spectrum
+    '''
+    def __init__(self, name, m_range, k_range, m_fn, f_comp, bias):
+        super(DiffuseComponent, self).__init__()
+        self.name = name
+        self.m_range = m_range
+        self.k_range = k_range
+        self.m_fn = m_fn
+        self.f_comp = f_comp
+        self.bias = bias
 
-#     @parameter
-#     def m_range(self, val):
-#         return val
+    #===========================================================================
+    # Parameters
+    #===========================================================================
+    @parameter
+    def name(self, val):
+        return val
 
-#     @parameter
-#     def k_range(self, val):
-#         return val
+    @parameter
+    def m_range(self, val):
+        return val
 
-#     @parameter
-#     def m_fn(self, val):
-#         if not isinstance(val, hmf.MassFunction):
-#             raise TypeError('m_fn should be hmf.MassFunction instance.')
-#         else: 
-#             return val
+    @parameter
+    def k_range(self, val):
+        return val
 
-#     @parameter
-#     def f_comp(self, val):
-#         return val
+    @parameter
+    def m_fn(self, val):
+        if not isinstance(val, hmf.MassFunction):
+            raise TypeError('m_fn should be hmf.MassFunction instance.')
+        else:
+            return val
 
-#     @parameter
-#     def bias(self, val):
-#         return val
-    
-#     #===========================================================================
-#     # Methods
-#     #===========================================================================    
-#     @cached_property('m_range', 'f_comp', 'm_fn')
-#     def rho_comp(self):
-#         '''
-#         Compute the average density of the component.
+    @parameter
+    def f_comp(self, val):
+        return val
 
-#             rho_comp(z) = int_m m n(m,z) f_comp(m,z)
-#         '''
-#         return Integrate(y=self.m_fn.dndlnm * self.f_comp,
-#                          x=self.m_range,
-#                          axis=0)
+    @parameter
+    def bias(self, val):
+        return val
 
-#     @cached_property('m_range', 'k_range', 'm_fn')
-#     def P_lin(self):
-#         '''
-#         Compute linear power spectrum
-#         '''
-#         return np.exp(self.m_fn.power)
-    
-#     @cached_property('P_lin', 'bias')
-#     def P_2h(self):
-#         '''
-#         Compute the 2-halo term of the power spectrum.
-#         '''
-#         return np.power(self.bias, 2) * self.P_lin
+    #===========================================================================
+    # Methods
+    #===========================================================================
+    @cached_property('m_range', 'f_comp', 'm_fn')
+    def rho_comp(self):
+        '''
+        Compute the average density of the component.
+
+            rho_comp(z) = int_m m n(m,z) f_comp(m,z)
+        '''
+        return Integrate(y=self.m_fn.dndlnm * self.f_comp,
+                         x=self.m_range,
+                         axis=0)
+
+    @cached_property('m_range', 'k_range', 'm_fn')
+    def P_lin(self):
+        '''
+        Compute linear power spectrum
+        '''
+        return np.exp(self.m_fn.power)
+
+    @cached_property('P_lin', 'bias')
+    def P_2h(self):
+        '''
+        Compute the 2-halo term of the power spectrum.
+        '''
+        return np.power(self.bias, 2) * self.P_lin
 
 
-#     @cached_property('k_range', 'P_1h')
-#     def Delta_2h(self):
-#         '''
-#         Return the dimensionless power spectrum for 2-halo term of component
-        
-#                Delta[k] = 1/(2*pi^2) * k^3 P(k)
-#         '''
-#         return 1./(2*np.pi**2) * self.k_range**3 * self.P_2h
-        
-# # ------------------------------------------------------------------------------
-# # End of DiffuseComponent()
-# # ------------------------------------------------------------------------------
+    @cached_property('k_range', 'P_1h')
+    def Delta_2h(self):
+        '''
+        Return the dimensionless power spectrum for 2-halo term of component
 
+               Delta[k] = 1/(2*pi^2) * k^3 P(k)
+        '''
+        return 1./(2*np.pi**2) * self.k_range**3 * self.P_2h
+
+# ------------------------------------------------------------------------------
+# End of DiffuseComponent()
+# ------------------------------------------------------------------------------

stars.py
@@ -6,7 +6,7 @@ import halo.parameters as p
 
 import pdb
 
-ddir = '/Volumes/Data/stijn/Documents/Universiteit/MR/code/halo/data/'
+ddir = '/Volumes/Data/stijn/Documents/Universiteit/MR/code/data/'
 
 def f_interp(m_range, f, m_f):
     '''
@@ -28,7 +28,7 @@ def f_interp(m_range, f, m_f):
     f_interp : (m,) array
       interpolated fractions
     '''
-    f_interp = intrp.interp1d(m_range, f, axis=0)
+    f_interp = intrp.interp1d(m_f, f, axis=0)
     mask = ((m_range < m_f[0]) & (m_range > m_f[-1]))
     f = np.zeros_like(m_range)
     f[~mask] = f_interp(m_range[~mask])
@@ -41,7 +41,7 @@ def f_interp(m_range, f, m_f):
 
 def mh_theory(m_cen):
     '''
-    Returns the mean halo mass for fixed central stellar mass m_cen, 
+    Returns the mean halo mass for fixed central stellar mass m_cen,
     using the iHOD model from Zu & Mandelbaum (2015).
 
         http://dx.doi.org/10.1093/mnras/stv2062
@@ -71,7 +71,7 @@ def mh_theory(m_cen):
 
 def mcen_theory(m_h):
     '''
-    Returns the central stellar mass m_s for the mean halo mass m_h, 
+    Returns the central stellar mass m_s for the mean halo mass m_h,
     using iHOD model from Zu & Mandelbaum (2015).
 
          http://dx.doi.org/10.1093/mnras/stv2062
@@ -99,7 +99,7 @@ def mcen_theory(m_h):
 
 def mh_fit(m_cen):
     '''
-    Returns the mean halo mass for fixed central stellar mass m_cen, using 
+    Returns the mean halo mass for fixed central stellar mass m_cen, using
     the iHOD model from Zu & Mandelbaum (2015), fitting function Eq. 59.
 
         http://dx.doi.org/10.1093/mnras/stv2062
@@ -125,7 +125,7 @@ def mh_fit(m_cen):
 
 def mcen_fit(m_h):
     '''
-    Returns the central stellar mass m_cen for the mean halo mass m_h, 
+    Returns the central stellar mass m_cen for the mean halo mass m_h,
     using the iHOD model from Zu & Mandelbaum (2015), inverse of eq. 59.
 
          http://dx.doi.org/10.1093/mnras/stv2062
@@ -155,8 +155,9 @@ def f_s(m_range):
     '''
     Satellite fraction from Zu & Mandelbaum
     '''
-    m_f, f, f_c, f_s = np.loadtxt(ddir + 'data_mccarthy/StellarFraction-Mh.txt',
-                                 unpack=True)
+    m_f, f, f_c, f_s = np.loadtxt(ddir +
+                                  'data_mccarthy/stars/StellarFraction-Mh.txt',
+                                  unpack=True)
     f_s_interp = f_interp(m_range, f_s, m_f)
 
     return f_s_interp
@@ -169,10 +170,11 @@ def f_c(m_range):
     '''
     Central fraction from Zu & Mandelbaum
     '''
-    m_f, f, f_c, f_s = np.loadtxt(ddir + 'data_mccarthy/StellarFraction-Mh.txt',
-                                 unpack=True)
+    m_f, f, f_c, f_s = np.loadtxt(ddir +
+                                  'data_mccarthy/stars/StellarFraction-Mh.txt',
+                                  unpack=True)
     f_c_interp = f_interp(m_range, f_c, m_f)
-    
+
     return f_c_interp
 
 # ------------------------------------------------------------------------------