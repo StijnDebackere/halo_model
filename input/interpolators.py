38fdd11ccbdedf6286092ce3cc0b4d2bdb4a4293
Stijn <debackere@strw.leidenuniv.nl>
2019-06-12 19:08:34 +0200

    Added mask interpolator, need to mask after interpolation!

--- input/interpolators.py
+++ input/interpolators.py
@@ -296,3 +296,69 @@ def fstar_500c_max_interp(f_c, sigma_lnc, m_file=None):
 # ------------------------------------------------------------------------------
 # End of fstar_500c_max_interp()
 # ------------------------------------------------------------------------------
+
+def gamma_max_interp(f_c, sigma_lnc, r_c, beta, r_flat, m_file=None):
+    '''
+    Return the interpolator for the gamma_max(z, m500c, fgas500c) relation
+    '''
+    if m_file is None:
+        fname_append = "_fc_{}_slnc_{}_rc_{}_beta_{}_rflat_{}.asdf".format(f_c, sigma_lnc,
+                                                                           r_c, beta, r_flat)
+        m_file = table_dir + "m500c_to_gamma_max" + fname_append
+
+    af = asdf.open(m_file)
+
+    if f_c != af.tree["f_c"]:
+        raise ValueError("f_c in {} is not {}".format(m_file, f_c))
+    if sigma_lnc != af.tree["sigma_lnc"]:
+        raise ValueError("sigma_lnc in {} is not {}".format(m_file, sigma_lnc))
+    # if r_c != af.tree["r_c"]:
+    #     raise ValueError("r_c in {} is not {}".format(m_file, r_c))
+    # if beta != af.tree["beta"]:
+    #     raise ValueError("beta in {} is not {}".format(m_file, beta))
+    # if r_flat != af.tree["r_flat"]:
+    #     raise ValueError("r_flat in {} is not {}".format(m_file, r_flat))
+
+    z = af.tree["z"][:]
+    m = af.tree["m500c"][:]
+    f = af.tree["fgas_500c"][:]
+    gamma_max = af.tree["gamma_max"][:]
+    
+    coords = (z, np.log10(m), f)
+
+    gamma_max_interp = interpolate.RegularGridInterpolator(coords, gamma_max)
+
+    return gamma_max_interp
+    
+# ------------------------------------------------------------------------------
+# End of gamma_max_interp()
+# ------------------------------------------------------------------------------
+
+def mask_interp(f_c, sigma_lnc, m_file=None):
+    '''
+    Return the interpolator for the mask
+    '''
+    if m_file is None:
+        m_file = table_dir + "m500c_to_m200m_dmo_fc_{}_slnc_{}.asdf".format(f_c,
+                                                                            sigma_lnc)
+
+    af = asdf.open(m_file)
+
+    if f_c != af.tree["f_c"]:
+        raise ValueError("f_c in {} is not {}".format(m_file, f_c))
+    if sigma_lnc != af.tree["sigma_lnc"]:
+        raise ValueError("sigma_lnc in {} is not {}".format(m_file, sigma_lnc))
+
+    z = af.tree["z"][:]
+    m = af.tree["m500c"][:]
+    f = af.tree["fgas_500c"][:]
+    mask = af.tree["mask"][:]
+
+    coords = (z, np.log10(m), f)
+    mask_interp = interpolate.RegularGridInterpolator(coords, mask)
+
+    return mask
+    
+# ------------------------------------------------------------------------------
+# End of mask_interp()
+# ------------------------------------------------------------------------------
