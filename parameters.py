b2bbac37ff38fd891bd38ac157efc9eb5833782c
Stijn <debackere@strw.leidenuniv.nl>
2022-04-14 14:24:35 +0200

    Modify parameter range.

--- parameters.py
+++ parameters.py
@@ -280,8 +280,8 @@ prms_planck = Parameters(m500c=np.logspace(10, 15, 101), logk_min=-1.,
                          cosmo=cosmo.Cosmology(default="planck_2019"))
 
 # fiducial parameters for halo mass project
-prms_masses = Parameters(m500c=np.logspace(14, 15.5, 100), logk_min=-1.)
-prms_masses_planck = Parameters(m500c=np.logspace(14, 15.5, 100), logk_min=-1.,
+prms_masses = Parameters(m500c=np.logspace(13, 15.5, 200), logk_min=-1.)
+prms_masses_planck = Parameters(m500c=np.logspace(13, 15.5, 200), logk_min=-1.,
                                 cosmo=cosmo.Cosmology(default="planck_2019"))
 
 # fiducial parameters, but with f_c = 1
